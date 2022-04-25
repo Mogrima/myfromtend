88c99bed43f4b0c7d28d25d08293566f1d0807c7
Mogrima <rebel.rs@yandex.ru>
2022-04-25 14:03:06 +0500

    анимированы кнопки в разделе навыки

--- src/js/main.js
+++ src/js/main.js
@@ -16,26 +16,39 @@
 //     }
 // }
 
-function animate(train) {
-    let start = Date.now();
+// class Animate {
+//   constructor() {
 
-    let timer = setInterval(function() {
-      let timePassed = Date.now() - start;
+//   }
+// }
 
-      train.style.left = timePassed / 5 + 'px';
+// function timer() {
+//   let start = Date.now();
+// }
 
-      if (timePassed > 2000) clearInterval(timer);
+// function animate(train) {
+//     let start = Date.now();
 
-    }, 20);
-}
+//     let timer = setInterval(function() {
+//       let timePassed = Date.now() - start;
 
-const obj1 = document.querySelector('.skill');
+//       train.style.bottom = timePassed / 25 + 'px';
+//       if (train.style.bottom == '160px') {
+//         train.style.top = timePassed / 25 + 'px';
+//       }
 
-obj1.addEventListener("click", function () {
-    animate(obj1)
-});
+//       if (timePassed > 4000) clearInterval(timer);
 
-console.log(obj1);
+//     }, 20);
+// }
+
+// const obj1 = document.querySelector('.skill');
+
+// obj1.addEventListener("click", function () {
+//     animate(obj1)
+// });
+
+// console.log(obj1);
 
 // train.onclick = function() {
 //     let start = Date.now();
@@ -49,3 +62,55 @@ console.log(obj1);
 
 //     }, 20);
 //   }
+
+const frontentBtn = document.getElementById('frontend')
+const backendBtn = document.getElementById('backend');
+const otherBtn = document.getElementById('something');
+const somethingdBtn = document.getElementById('other');
+
+let frontendItem = document.querySelectorAll('.frontend');
+let backendItem = document.querySelectorAll('.back');
+let somethingItem = document.querySelectorAll('.something');
+let otherItem = document.querySelectorAll('.other');
+
+function timeRemoveClass(elem) {
+  elem.classList.remove('bounceIn');
+  elem.classList.remove('hoverButton');
+  console.log('time');
+}
+
+frontentBtn.addEventListener('click', () => {
+  for (i = 0; i < frontendItem.length; i++) {
+    frontendItem[i].classList.add('bounceIn');
+    frontendItem[i].classList.add('hoverButton');
+    setTimeout(timeRemoveClass, 900, frontendItem[i]);
+  }
+  
+})
+
+backendBtn.addEventListener('click', () => {
+  for (i = 0; i < backendItem.length; i++) {
+    backendItem[i].classList.add('bounceIn');
+    backendItem[i].classList.add('hoverButton');
+    setTimeout(timeRemoveClass, 900, backendItem[i]);
+  }
+  
+})
+
+otherBtn.addEventListener('click', () => {
+  for (i = 0; i < somethingItem.length; i++) {
+    somethingItem[i].classList.add('bounceIn');
+    somethingItem[i].classList.add('hoverButton');
+    setTimeout(timeRemoveClass, 900, somethingItem[i]);
+  }
+  
+})
+
+somethingdBtn.addEventListener('click', () => {
+  for (i = 0; i < otherItem.length; i++) {
+    otherItem[i].classList.add('bounceIn');
+    otherItem[i].classList.add('hoverButton');
+    setTimeout(timeRemoveClass, 900, otherItem[i]);
+  }
+  
+})
