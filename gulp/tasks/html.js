ac35b2fab60f3f0d97de79c9c225b107eed4c8fa
Mogrima <rebel.rs@yandex.ru>
2022-04-25 12:02:45 +0500

    из запуска gulp убран gulp-w3c-html-validator, который валил все сборку

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp')
 const plumber = require('gulp-plumber')
-const htmlValidator = require('gulp-w3c-html-validator')
+// const htmlValidator = require('gulp-w3c-html-validator')
 
 module.exports = function html() {
   return gulp.src('src/*.html')
     .pipe(plumber())
-    .pipe(htmlValidator())
+    // .pipe(htmlValidator())
     .pipe(gulp.dest('build'))
 }
 
